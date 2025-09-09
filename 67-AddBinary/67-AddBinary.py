8c5218c4c6f8857cbc68f2b70155df4a7b184aa0
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:20:13 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.py
@@ -0,0 +1,24 @@
+# Last updated: 09/09/2025, 14:20:15
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i, j = len(a) - 1, len(b) - 1  
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+        
+            result.append(str(total % 2))
+            carry = total // 2
+        return "".join(reversed(result))
