2ad297b9cb154db69fea107432c9a7c7c69108e8
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:20:17 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,14 @@
+# Last updated: 09/09/2025, 14:20:20
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0  
+        return [1] + [0] * n
