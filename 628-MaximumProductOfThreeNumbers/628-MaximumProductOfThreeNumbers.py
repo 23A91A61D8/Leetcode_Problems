f65625fbb0776552bc8b0fe671e6ce4e69d36874
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:18:44 +0530

    Create 628-MaximumProductOfThreeNumbers

---
+++ 628-MaximumProductOfThreeNumbers/628-MaximumProductOfThreeNumbers.py
@@ -0,0 +1,9 @@
+# Last updated: 09/09/2025, 14:18:46
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
