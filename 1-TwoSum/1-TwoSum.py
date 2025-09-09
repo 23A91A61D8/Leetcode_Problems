ca5c8c647aaafd47aa02bd67728e0f67080c6809
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:20:42 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,16 @@
+# Last updated: 09/09/2025, 14:20:44
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+        
