efa03297e0ee5354d94c4ea27d74e73a1e48b972
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:19:13 +0530

    Create 500-KeyboardRow

---
+++ 500-KeyboardRow/500-KeyboardRow.py
@@ -0,0 +1,14 @@
+# Last updated: 09/09/2025, 14:19:15
+class Solution:
+    def findWords(self, words):
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        
+        result = []
+        for word in words:
+            word_set = set(word.lower())
+            if word_set.issubset(row1) or word_set.issubset(row2) or word_set.issubset(row3):
+                result.append(word)
+        
+        return result
