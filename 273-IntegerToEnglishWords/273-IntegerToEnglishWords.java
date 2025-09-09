c1d4dd102635dc41207ddfa39d0ce14d3b1315cf
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:19:20 +0530

    Create 273-IntegerToEnglishWords

---
+++ 273-IntegerToEnglishWords/273-IntegerToEnglishWords.java
@@ -0,0 +1,46 @@
+// Last updated: 09/09/2025, 14:19:22
+public class Solution {
+    private static final String[] BELOW_20 = {
+        "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    private static final String[] TENS = {
+        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    private static final String[] THOUSANDS = {
+        "", "Thousand", "Million", "Billion"
+    };
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        
+        String words = "";
+        int i = 0;
+        
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
+        }
+        
+        return words.trim();
+    }
+    
+    private String helper(int num) {
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return BELOW_20[num] + " ";
+        } else if (num < 100) {
+            return TENS[num / 10] + " " + helper(num % 10);
+        } else {
+            return BELOW_20[num / 100] + " Hundred " + helper(num % 100);
+        }
+    }
+}
