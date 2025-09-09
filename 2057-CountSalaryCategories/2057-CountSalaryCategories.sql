23dd8e600656cd5475bb8ac4f0c1437436750406
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-09 14:17:17 +0530

    Create 2057-CountSalaryCategories

---
+++ 2057-CountSalaryCategories/2057-CountSalaryCategories.sql
@@ -0,0 +1,19 @@
+-- Last updated: 09/09/2025, 14:17:19
+SELECT 'Low Salary' AS category, 
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+
+SELECT 'Average Salary' AS category, 
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
+
+UNION ALL
+
+SELECT 'High Salary' AS category, 
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000;
