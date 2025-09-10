902697b9164febc3edff3ec0b9036e608df04015
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-10 17:47:49 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,13 @@
+# Last updated: 10/09/2025, 17:47:48
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
