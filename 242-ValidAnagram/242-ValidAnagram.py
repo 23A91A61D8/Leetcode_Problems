a9820efa0753ebe91dfd2d17749749c0e61b21e3
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2025-09-16 21:19:04 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,9 @@
+# Last updated: 16/09/2025, 21:19:06
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s)==sorted(t)
