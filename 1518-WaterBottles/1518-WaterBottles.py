980895d8c6fb096b8c8d9a1bbbf035be395da6c5
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-01-06 22:05:35 +0530

    File updated at 06/01/2026, 22:05:36

--- 1518-WaterBottles/1518-WaterBottles.py
+++ 1518-WaterBottles/1518-WaterBottles.py
@@ -1,13 +1,12 @@
-# Last updated: 06/01/2026, 22:05:16
+# Last updated: 06/01/2026, 22:05:36
 1class Solution(object):
 2    def numWaterBottles(self, numBottles, numExchange):
 3        total = numBottles
 4        empty = numBottles
-5
-6        while empty >= numExchange:
-7            new_bottles = empty // numExchange
-8            total += new_bottles
-9            empty = new_bottles + (empty % numExchange)
-10
-11        return total
-12
+5        while empty >= numExchange:
+6            new_bottles = empty // numExchange
+7            total += new_bottles
+8            empty = new_bottles + (empty % numExchange)
+9
+10        return total
+11
