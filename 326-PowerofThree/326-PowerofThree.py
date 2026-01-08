a3a9091496920796c3719d4f5f99accadc736711
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-01-08 22:27:16 +0530

    Create 326-PowerofThree

---
+++ 326-PowerofThree/326-PowerofThree.py
@@ -0,0 +1,9 @@
+# Last updated: 08/01/2026, 22:27:15
+1class Solution(object):
+2    def isPowerOfThree(self, n):
+3        if n <= 0:
+4            return False
+5        while n % 3 == 0:
+6            n //= 3
+7        return n == 1
+8
