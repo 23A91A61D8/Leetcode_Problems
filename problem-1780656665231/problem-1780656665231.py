bd581e9d43e9f14befbdcd9a13ce72873db5f53f
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-05 16:20:59 +0530

    Create

---
+++ problem-1780656665231/problem-1780656665231.py
@@ -0,0 +1,13 @@
+# Last updated: 05/06/2026, 16:21:05
+1class Solution(object):
+2    def findTheDifference(self, s, t):
+3        """
+4        :type s: str
+5        :type t: str
+6        :rtype: str
+7        """
+8        for ch in t:
+9            if t.count(ch) != s.count(ch):
+10                return ch
+11
+12        
