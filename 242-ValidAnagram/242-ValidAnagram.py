01dbee594c7e6c4991159aac284fb19374ce309b
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-09 15:51:18 +0530

    File updated at 09/06/2026, 15:51:31

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,9 +1,13 @@
-# Last updated: 16/09/2025, 21:19:06
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        return sorted(s)==sorted(t)
+# Last updated: 09/06/2026, 15:51:31
+1class Solution(object):
+2    def isAnagram(self, s, t):
+3        """
+4        :type s: str
+5        :type t: str
+6        :rtype: bool
+7        """
+8        if sorted(s) == sorted(t):
+9            return True
+10        else:
+11            return False
+12        
