0ca9a66f7c872c46cf513999e1a99abb845aea1c
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-10 12:23:29 +0530

    File updated at 10/06/2026, 12:23:44

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,10 +1,13 @@
-# Last updated: 26/12/2025, 23:03:29
+# Last updated: 10/06/2026, 12:23:44
 1class Solution(object):
 2    def majorityElement(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: int
-6        """
-7        for num in set(nums):
-8            if nums.count(num) > len(nums)//2:
-9                return num
+3        count = 0
+4        candidate = None
+5        for num in nums:
+6            if count == 0:
+7                candidate = num
+8            if num == candidate:
+9                count += 1
+10            else:
+11                count -= 1
+12        return candidate
