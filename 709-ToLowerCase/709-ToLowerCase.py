063a9c5567dfe3ba9daf2fcfb628a0f287c1c97b
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-12 21:56:52 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.py
@@ -0,0 +1,9 @@
+# Last updated: 12/06/2026, 21:56:53
+1class Solution(object):
+2    def toLowerCase(self, s):
+3        """
+4        :type s: str
+5        :rtype: str
+6        """
+7        return lower(s)
+8        
