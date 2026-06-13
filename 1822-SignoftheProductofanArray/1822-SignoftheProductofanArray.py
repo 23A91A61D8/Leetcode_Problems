58796b86614db5c70fd7b66db2404360e657bd37
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-13 22:31:12 +0530

    Create 1822-SignoftheProductofanArray

---
+++ 1822-SignoftheProductofanArray/1822-SignoftheProductofanArray.py
@@ -0,0 +1,16 @@
+# Last updated: 13/06/2026, 22:31:13
+1class Solution(object):
+2    def arraySign(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        for num in nums:
+8            if num == 0:
+9                return 0
+10        negative_count = 0
+11        for num in nums:
+12            if num < 0:
+13                negative_count += 1
+14        return 1 if negative_count % 2 == 0 else -1
+15
