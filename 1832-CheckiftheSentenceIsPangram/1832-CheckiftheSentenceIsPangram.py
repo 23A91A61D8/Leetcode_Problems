32de026f337b259c80782e6f369e531a2651db1e
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-18 21:48:44 +0530

    Create 1832-CheckiftheSentenceIsPangram

---
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.py
@@ -0,0 +1,12 @@
+# Last updated: 18/06/2026, 21:48:52
+1class Solution(object):
+2    def checkIfPangram(self, sentence):
+3        """
+4        :type sentence: str
+5        :rtype: bool
+6        """
+7        letters = set()
+8        for ch in sentence:
+9            if ch.isalpha():
+10                letters.add(ch.lower())
+11        return len(letters) == 26        
