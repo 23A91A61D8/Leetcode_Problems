c34d71bf910c06c189fabc39ab2f81e171f4b257
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-25 22:21:46 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.py
@@ -0,0 +1,14 @@
+# Last updated: 25/06/2026, 22:21:47
+1class Solution(object):
+2    def addStrings(self, num1, num2):
+3        """
+4        :type num1: str
+5        :type num2: str
+6        :rtype: str
+7        """
+8        result = " "
+9        num1 = int(num1)
+10        num2 = int(num2)
+11        result = num1+num2
+12        return str(result)
+13        
