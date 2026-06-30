b64e0f666a99039c07b5be0a667ba98cad09e815
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-30 21:42:39 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.py
@@ -0,0 +1,9 @@
+# Last updated: 30/06/2026, 21:42:41
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return lower(s)
+        
