656579b4ecfe37f7b6ce3d173c5abe39e1fdc76d
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-06-30 21:43:40 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.py
@@ -0,0 +1,17 @@
+# Last updated: 30/06/2026, 21:43:42
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        last_index = {}
+
+        for i in range(len(nums)):
+            if nums[i] in last_index:
+                if i - last_index[nums[i]] <= k:
+                    return True
+            last_index[nums[i]] = i
+
+        return False
