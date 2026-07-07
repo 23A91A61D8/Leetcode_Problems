106289e3c304b57f213e4cef4d9aa840af074434
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-07-07 22:30:25 +0530

    File updated at 07/07/2026, 22:30:27

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,11 +1,11 @@
-# Last updated: 30/06/2026, 21:43:33
-class Solution(object):
-    def moveZeroes(self, nums):
-        insert_pos = 0
-        for num in nums:
-            if num != 0:
-                nums[insert_pos] = num
-                insert_pos += 1
-        while insert_pos < len(nums):
-            nums[insert_pos] = 0
-            insert_pos += 1
+# Last updated: 07/07/2026, 22:30:27
+1class Solution(object):
+2    def moveZeroes(self, nums):
+3        insert_pos = 0
+4        for num in nums:
+5            if num != 0:
+6                nums[insert_pos] = num
+7                insert_pos += 1
+8        while insert_pos < len(nums):
+9            nums[insert_pos] = 0
+10            insert_pos += 1
