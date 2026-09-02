d9321763be0b695d41faba1048345fcc3b301733
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-09-02 21:48:42 +0530

    Create 3875-ConstructUniformParityArrayI

---
+++ 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.py
@@ -0,0 +1,11 @@
+# Last updated: 02/09/2026, 21:48:43
+1class Solution(object):
+2    def uniformArray(self, nums1):
+3        """
+4        :type nums1: List[int]
+5        :rtype: bool
+6        """
+7        return True
+8
+9
+10        
