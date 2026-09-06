8ed5925cb8de5ac05ec08e22552e353950d81a5c
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-09-06 18:25:38 +0530

    Create 2843-CountSymmetricIntegers

---
+++ 2843-CountSymmetricIntegers/2843-CountSymmetricIntegers.py
@@ -0,0 +1,13 @@
+# Last updated: 06/09/2026, 18:25:44
+1class Solution(object):
+2    def countSymmetricIntegers(self, low, high):
+3        count = 0
+4        for num in range(low, high + 1):
+5            s = str(num)
+6            n = len(s)
+7            if n % 2 == 0:
+8                mid = n // 2
+9                if sum(map(int, s[:mid])) == sum(map(int, s[mid:])):
+10                    count += 1
+11        return count
+12
