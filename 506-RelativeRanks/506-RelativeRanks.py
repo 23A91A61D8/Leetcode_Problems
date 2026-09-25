5dd349a4df29d9abeae3fa6b0657ff20174d3fe4
23A91A61D8 <147516111+23A91A61D8@users.noreply.github.com>
2026-09-25 21:12:21 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.py
@@ -0,0 +1,16 @@
+# Last updated: 25/09/2026, 21:12:27
+1class Solution(object):
+2    def findRelativeRanks(self, score):
+3        sorted_score = sorted(score, reverse=True)
+4        rank = {}
+5        for i in range(len(sorted_score)):
+6            if i == 0:
+7                rank[sorted_score[i]] = "Gold Medal"
+8            elif i == 1:
+9                rank[sorted_score[i]] = "Silver Medal"
+10            elif i == 2:
+11                rank[sorted_score[i]] = "Bronze Medal"
+12            else:
+13                rank[sorted_score[i]] = str(i + 1)
+14        return [rank[x] for x in score]
+15
